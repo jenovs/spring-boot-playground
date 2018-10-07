0150c50ba2a8ef7e312bbf3d61369d8958245185
Viktors Jenovs <jviktors@gmail.com>
2018-10-07 19:57:02 +0300

    Code cleanup

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -1,19 +1,18 @@
 package todo;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
-  private final AtomicLong id = new AtomicLong();
   private ArrayList<Todo> todos = new ArrayList<Todo>();
 
   @GetMapping("/")
